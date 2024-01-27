463d8bbee68ef0204fa91d9590f736dc0c9a4cd5
Omer <omer.dayan4u@gmail.com>
2024-01-27 15:18:10 +0200

    Memory and a bit of arithmatics

---
+++ project07/CodeWriter.py
@@ -0,0 +1,142 @@
+class Writer:
+    op_num = 0
+    filename = ""
+    asm_code = ""
+
+    segments = {"local": "LCL",
+                "argument": "ARG",
+                "this": "THIS",
+                "that": "THAT"}
+
+    def __init__(self, file):
+        self.op_num = 0
+        self.filename = file
+        self.asm_code = ""
+
+    def write_arithmetics(self, command: str, op_num=0):
+        self.asm_code += f"{command}"
+        # add
+        if command == "add":
+            self.asm_code += (f"@SP\n"
+                              f"M=M-1\n"
+                              f"A=M\n"
+                              f"D=M\n"
+                              f"A=A-1\n"
+                              f"D=D+M\n"
+                              f"M=D\n")
+        # sub
+        elif command == "sub":
+            self.asm_code += (f"@SP\n"
+                              f"M=M-1\n"
+                              f"A=M\n"
+                              f"D=-M\n"
+                              f"A=A-1\n"
+                              f"D=D+M\n"
+                              f"M=D\n")
+        # neg
+        elif command == "neg":
+            self.asm_code += (f"@SP\n"
+                              f"A=M-1\n"
+                              f"M=-M\n")
+        # eq
+        elif command == "eq":
+            pass
+        # gt
+        elif command == "gt":
+            pass
+        # lt
+        elif command == "lt":
+            pass
+        # And
+        elif command == "and":
+            pass
+        # Or
+        elif command == "or":
+            pass
+        # not
+        else:
+            pass
+        pass
+
+    def write_push_pop(self, command: str, segment: str, index: int):
+        asm_code = f"// {command} {segment} {index}\n"
+        if command == "C_POP":
+
+            # Temporarily change ARG/LCL/THIS/THAT to the addition
+            if segment in self.segments.keys():
+                self.asm_code += (f"@{index}\n"
+                                  f"D=A\n"
+                                  f"@{self.segments[segment]}\n"
+                                  f"M=M+D\n")
+
+            # Remove from stack
+            self.asm_code += (f"@SP\n"
+                              f"M=M-1\n"
+                              f"A=M\n"
+                              f"D=M\n")
+
+            # Put in temp block
+            if segment == "temp":
+                self.asm_code += (f"@{5 + index}\n"
+                                  f"M=D\n")
+
+            # Put in THIS/THAT
+            elif segment == "pointer":
+                if index == 0:
+                    self.asm_code += "@THIS\n"
+                else:
+                    self.asm_code += "@THAT\n"
+                self.asm_code += "M=D\n"
+
+            # Put in @filname.i
+            elif segment == "static":
+                self.asm_code += (f"@{self.filename}.{index}\n"
+                                  f"M=D")
+
+            # Insert value and remove the index back
+            elif segment in self.segments.keys():
+                self.asm_code += (f"@{self.segments[segment]}\n"
+                                  f"A=M\n"
+                                  f"M=D\n"
+                                  f"@{index}\n"
+                                  f"D=A\n"
+                                  f"@{self.segments[segment]}\n"
+                                  f"M=M-D\n")
+
+        elif command == "C_PUSH":
+            # Get value from ARG/THIS/THAT/LCL
+            if segment in self.segments.keys():
+                self.asm_code += (f"@{index}\n"
+                                  f"D=A\n"
+                                  f"@{self.segments[segment]}\n"
+                                  f"D=D+M\n"
+                                  f"A=D\n"
+                                  f"D=M\n")
+
+            # Get value from 5+index
+            elif segment == "temp":
+                self.asm_code += (f"@{5 + index}\n"
+                                  f"D=M\n")
+
+            # Get @THIS/THAT content
+            elif segment == "pointer":
+                if index == 0:
+                    self.asm_code += "@THIS\n"
+                else:
+                    self.asm_code += "@THAT\n"
+                self.asm_code += "D=M"
+
+            elif segment == "static":
+                self.asm_code += (f"@{self.filename}.{index}\n"
+                                  f"D=M")
+
+            # Add D to stack and raise by 1
+            self.asm_code += (f"@SP\n"
+                              f"A=M\n"
+                              f"M=D\n"
+                              f"@SP\n"
+                              f"M=M+1\n")
+
+
+def close():
+    pass
---
+++ project07/Parser.py
@@ -0,0 +1,19 @@
+class Parser:
+
+    def __init__(self):
+        pass
+
+    def has_more_lines(self):
+        pass
+
+    def advance(self):
+        pass
+
+    def command_type(self):
+        pass
+
+    def arg1(self):
+        pass
+
+    def arg2(self):
+        pass
