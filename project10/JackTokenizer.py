ad68e02f283bb894c9dd899132e423d65fd0448f
Omer <omer.dayan4u@gmail.com>
2024-02-07 21:20:11 +0200

    Real skeleton project 10

---
+++ project10/JackTokenizer.py
@@ -0,0 +1,31 @@
+class Tokenizer:
+
+    current_token = ""
+    remaining_code = ""
+
+    def __init__(self):
+        pass
+
+    def has_more_tokens(self):
+        pass
+
+    def advance(self):
+        pass
+
+    def token_type(self):
+        pass
+
+    def keyword(self):
+        pass
+
+    def symbol(self):
+        pass
+
+    def identifier(self):
+        pass
+
+    def int_val(self):
+        pass
+
+    def string_val(self):
+        pass
