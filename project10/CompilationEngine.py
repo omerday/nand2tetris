3b84c1fdca4bc3dd6f1c4688568d0ec8a73ed3ef
Omer <omer.dayan4u@gmail.com>
2024-02-07 21:19:44 +0200

    Skeleton

---
+++ project10/CompilationEngine.py
@@ -0,0 +1,42 @@
+class Compiler:
+
+    tokenizer = None
+
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+
+    def compile_class(self):
+        pass
+
+    def compile_class_var_dec(self):
+        pass
+
+    def compile_subroutine(self):
+        pass
+
+    def compile_parameter_list(self):
+        pass
+
+    def compile_subroutine_body(self):
+        pass
+
+    def compile_var_dec(self):
+        pass
+
+    def compile_statement(self):
+        pass
+
+    def compile_let(self):
+        pass
+
+    def compile_if(self):
+        pass
+
+    def compile_while(self):
+        pass
+
+    def compile_do(self):
+        pass
+
+    def compile_return(self):
+        pass
